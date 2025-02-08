c28d731a2acd4a85a1e0314af0f99760a1fb9743
jayflynndev <jayflynndev@outlook.com>
2025-02-08 17:31:07 +0000

    0.17

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,8 +8,8 @@ export default function Footer() {
     <footer className="w-full bg-gradient-to-t from-purple-400 to-purple-800 p-4">
       <div className="container mx-auto text-center text-white flex flex-col items-center">
         <p>
-          Find more quizzes at the socials below, and don't forget to follow as
-          well!
+          Find more quizzes at the socials below, and don&apos;t forget to
+          follow as well!
         </p>
         <div className="flex space-x-4 mt-2">
           <a
